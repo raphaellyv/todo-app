bdc3847b2e7c0054db16f5c0a4fe95e1b6302a9d
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-08-29 23:04:32 -0300

    Add style to input and button

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,4 +1,5 @@
 import toDoIcon from "../assets/todo-icon.svg"
+
 function ToDo() {
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
@@ -7,9 +8,22 @@ function ToDo() {
         <h1 className="text-3xl font-semibold">To-Do List</h1>
       </div>
 
+      <div className="flex items-center my-7 bg-gray-200 rounded-full">
+        <input
+          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          type="text"
+          placeholder="Add your task"
+        >
+        </input>
+        <button
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
+          ADD +
+        </button>
+      </div>
+
       <div>
-        <input type="text" placeholder="Add your task" ></input>
-        <button>ADD +</button>
+
       </div>
     </div>
   )

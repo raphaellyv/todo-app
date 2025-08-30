0f2dc66463d4b9fd4418840c9bab541ba0bf50a0
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-08-30 17:21:46 -0300

    Add add function

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,7 +1,28 @@
-import todo_icon from "../assets/todo_icon.svg"
-import ToDoItems from "./ToDoItems"
+import { useRef, useState } from "react";
+import todo_icon from "../assets/todo_icon.svg";
+import ToDoItems from "./ToDoItems";
 
 function ToDo() {
+  const [toDoList, setToDoList] = useState([])
+  const inputRef = useRef();
+
+  function add() {
+    const inputText = inputRef.current.value.trim();
+
+    if(inputText === "") {
+      return null;
+    }
+
+    const newToDo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+
+    setToDoList([...toDoList, newToDo]);
+    inputRef.current.value = "";
+  }
+
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       <div className="flex items-center mt-7 gap-2">
@@ -11,6 +32,7 @@ function ToDo() {
 
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+          ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add your task"
@@ -18,13 +40,18 @@ function ToDo() {
         </input>
         <button
           className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+          onClick={add}
         >
           ADD +
         </button>
       </div>
 
       <div>
-        <ToDoItems/>
+        {toDoList.map((item, index)=>{
+          return (
+            <ToDoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete}/>
+          )
+        })}
       </div>
     </div>
   )
--- src/components/ToDoItems.jsx
+++ src/components/ToDoItems.jsx
@@ -2,12 +2,12 @@ import tick from "../assets/tick.svg";
 import not_tick from "../assets/not_tick.svg";
 import delete_icon from "../assets/delete_icon.svg";
 
-function ToDoItems() {
+function ToDoItems({text, id, isComplete}) {
   return (
     <div className="flex items-center my-3 gap-2">
       <div className="flex flex-1 items-center cursor-pointer">
         <img className="w-7" src={tick} alt=""/>
-        <p className="text-slate-700 ml-4 text-[17px]">sdfs</p>
+        <p className="text-slate-700 ml-4 text-[17px]">{text}</p>
       </div>
 
       <img className="w-4.5 cursor-pointer" src={delete_icon} alt=""/>
